c0ce4800327a617293924ac65cf06321b1657ea7
i-X-ce <iceci2804@gmail.com>
2025-11-10 21:11:37 +0900

    🏷️ 定数の追加

---
+++ consts/VIEW.ts
@@ -0,0 +1,13 @@
+export const VIEWS = {
+  ANIMATION: "animation",
+  UI: "ui",
+  ORDER: "order",
+};
+
+export const UI_VIEWS = {
+  BUTTON: "button",
+  SWITCH: "switch",
+  MODAL: "modal",
+  TABS: "tabs",
+  ACCORDION: "accordion",
+};
