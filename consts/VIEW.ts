d5b4688ccf1ee7f5f67ddc4a6abf0f6a7e03cce4
i-X-ce <iceci2804@gmail.com>
2025-11-10 21:19:18 +0900

    ✨ 種類をいろいろ追加&名前を変更

--- consts/VIEW.ts
+++ consts/VIEW.ts
@@ -1,13 +1,30 @@
-export const VIEWS = {
-  ANIMATION: "animation",
-  UI: "ui",
-  ORDER: "order",
+export const VIEWS_TYPE = {
+  EASING: "easing", // イージング
+  UI: "ui", // UI
+  ORDER: "order", // 順序
+  LAW: "law", // 法則
 };
 
-export const UI_VIEWS = {
+export const UI_TYPE = {
   BUTTON: "button",
   SWITCH: "switch",
   MODAL: "modal",
   TABS: "tabs",
   ACCORDION: "accordion",
+};
+
+export const ORDER_TYPE = {
+  ABOVE: "above", // 上から
+  BELOW: "below", // 下から
+  LEFT: "left", // 左から
+  RIGHT: "right", // 右から
+  DESCENDING: "descending", // 降順
+  ASCENDING: "ascending", // 昇順
+};
+
+export const LAW_TYPE = {
+  GRAVITY: "gravity", // 重力
+  SPRING: "spring", // バネ
+  FRICTION: "friction", // 摩擦
+  PREPARATORY: "preparatory", // 予備動作
 };
